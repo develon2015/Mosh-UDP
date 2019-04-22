3b5f09993a01d9a2a15b571e66e4ea54a274f9a3
Develon's Topmain <develon@qq.com>
2019-04-22 13:11:22 +0800

    up

--- mosh-udp-server.c
+++ mosh-udp-server.c
@@ -12,14 +12,24 @@
 #include <termios.h>
 #include <unistd.h>
 #include <sys/param.h>
+#include <fcntl.h>
+#include <wait.h>
 
 char **Argv = NULL;
 
 void 
 init_daemon() {
 	close(STDOUT_FILENO);
-	close(STDIN_FILENO);
-	close(STDERR_FILENO);
+	//close(STDIN_FILENO);
+	//close(STDERR_FILENO);
+	int out = open("/dev/null", O_WRONLY);
+	if (out < 0) {
+		perror("打开/dev/null失败");
+	}
+	if (dup2(STDOUT_FILENO, out) != out) {
+		perror("重定向失败");
+	}
+	close(out);
 	// 子进程不再继承终端IO
 
 	int pid = fork();
@@ -78,29 +88,29 @@ main(int argc, char *argv[]) {
 	}
 	Argv = argv;
 	if (strcmp("--LOGIN", argv[1]) == 0) {
-			int fd0, fd1;
-			sscanf(argv[2], "%d", &fd0);
-			sscanf(argv[3], "%d", &fd1);
-			close(fd0);
-			printf("fd0, 1 -> %d %d\n", fd0, fd1);
-			char buf[1024] = { 0 };
-			while (strlen(buf) < 16) // mosh-server返回空行数量因版本而异, 故修复bug
-				fgets(buf, 1024, stdin); //huh
-			printf("buf -> %s\n", buf);
-			int port;
-			char key[1024];
-			if (sscanf(buf, "%*s %*s %d %s", &port, key) != 2) {
-				// 启动失败, 可能是端口被占用
-				printf("[E] 启动失败, 可能是端口被占用\n");
-				close(fd1);
-				return 0;
-			}
-			char output[1024] = { 0 };
-			sprintf(output, "GO! %d %s", port, key);
-			printf("output -> %s\n", output);
-			printf("[I] write %ld\n", write(fd1, output, strlen(output)) );
+		int fd0, fd1;
+		sscanf(argv[2], "%d", &fd0);
+		sscanf(argv[3], "%d", &fd1);
+		close(fd0);
+		printf("fd0, 1 -> %d %d\n", fd0, fd1);
+		char buf[1024] = { 0 };
+		while (strlen(buf) < 16) // mosh-server返回空行数量因版本而异, 故修复bug
+			fgets(buf, 1024, stdin); //huh
+		printf("buf -> %s\n", buf);
+		int port;
+		char key[1024];
+		if (sscanf(buf, "%*s %*s %d %s", &port, key) != 2) {
+			// 启动失败, 可能是端口被占用
+			printf("[E] 启动失败, 可能是端口被占用\n");
 			close(fd1);
 			return 0;
+		}
+		char output[1024] = { 0 };
+		sprintf(output, "GO! %d %s", port, key);
+		printf("output -> %s\n", output);
+		printf("[I] write %ld\n", write(fd1, output, strlen(output)) );
+		close(fd1);
+		return 0;
 	}
 	printf("欢迎使用Mosh登录器-server, 访问主页: https://github.com/develon2015/Mosh-UDP\n");
 	int port = 6666; /* 监听端口 */
@@ -139,7 +149,8 @@ main(int argc, char *argv[]) {
 	else
 		puts("您当前处于调试模式");
 
-	while (1) {
+L_while:
+	{
 		char buf[1024 + 8] = { 0 };
 		struct sockaddr_in client = { 0 };
 		socklen_t len = sizeof client;
@@ -147,28 +158,49 @@ main(int argc, char *argv[]) {
 		recvfrom(sfd, buf, 1024, 0, (struct sockaddr *)&client, &len);
 		printf("[%s : %d]\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
 		char buf_login_magic[1024] = { 0 },
-			buf_passwd[1024] = { 0 },
-			buf_check[1024] = { 0 };
+		     buf_passwd[1024] = { 0 },
+		     buf_check[1024] = { 0 };
 		int session_port = 0;
 		if (sscanf(buf, "%s %s %d %s", buf_login_magic, buf_passwd, &session_port, buf_check) != 4) {
-GETOUT:
-			sendto(sfd, "GET OUT!", 9, 0, (void *)&client, len);
-			continue;
+			const char *RS = "GET OUT!";
+			sendto(sfd, RS, strlen(RS) + 1, 0, (void *)&client, len);
+			goto L_while;
 		}
-		if (strcmp("LOGIN", buf_login_magic) != 0 || strcmp("0xArimuraKasumi", buf_check) != 0 || strcmp(passwd, buf_passwd) != 0)
-			goto GETOUT;
-		
+		if (strcmp("LOGIN", buf_login_magic) != 0 || strcmp("0xArimuraKasumi", buf_check) != 0 || strcmp(passwd, buf_passwd) != 0) {
+			const char * RS = "拒绝登录, 可能是您的密码错误";
+			sendto(sfd, RS, strlen(RS) + 1, 0, (void *)&client, len);
+			goto L_while;
+		}
+
+		// 验证成功, 启动moser-server
+		switch (fork()) {
+			case 0:
+				switch (fork()) {
+					case 0:
+						break;
+					case -1:
+					default:
+						return 0;
+				}
+				break;
+			case -1:
+			default:
+				wait(NULL);
+				goto L_while;
+		}
+
 		int mosh_port = 0;
 		char mosh_key[1024] = { 0 };
 		char mosh_buf[1024] = { 0 };
 		if (startMosh(session_port, &mosh_port, mosh_key) != 0) {
-			printf("start mosh failed\n");
-			goto GETOUT;
+			const char *RS = "服务器无法启动mosh-server";
+			sendto(sfd, RS, strlen(RS) + 1, 0, (void *)&client, len);
+			return 0;
 		}
 		sprintf(mosh_buf, "GO! %d %s #", mosh_port, mosh_key);
 		// 返回"GO! 端口号 KEY #"
 		sendto(sfd, mosh_buf, strlen(mosh_buf)+1, 0, (void *)&client, len);
 	}
-	
+
 	return 0;
 }
